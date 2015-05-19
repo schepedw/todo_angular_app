aa2944380652f121598a953520373e4b2c934a61
Daniel Schepers <schepedw@gmail.com>
2015-05-19 14:18:41 -0500

    done :)

--- todo.js
+++ todo.js
@@ -2,9 +2,21 @@
 var app = angular.module('Todo', []);
 
 app.controller('TodoCtrl', function($scope) {
-    
-    $scope.todos = [
-            {text:'Learn AngularJS', done:false},
-            {text:'Build an app', done:false}
-    ];
+
+  $scope.todos = [
+    {text:'Learn AngularJS', done:true},
+    {text:'Build an app', done:false}
+  ];
+  $scope.addTodo = function(){
+    $scope.todos.push({text: $scope.newTodo, done: false});
+  }
+
+  $scope.removeCompleted = function(){
+    for (var i =0; i < $scope.todos.length; i++){
+      if ($scope.todos[i].done){
+        $scope.todos.splice(i, 1);
+      }
+    }
+
+  }
 });
