03baff26e45390023aaed3c022c8fdb9ee46e385
Gopi Guntupalli <gguntupalli@enova.com>
2015-05-19 01:50:34 -0500

    solution

--- todo.js
+++ todo.js
@@ -7,4 +7,15 @@ app.controller('TodoCtrl', function($scope) {
             {text:'Learn AngularJS', done:false},
             {text:'Build an app', done:false}
     ];
+
+    $scope.clearCompleted = function () {
+        $scope.todos = $scope.todos.filter(function(todo){
+            return !todo.done;
+        });
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.TodoText, done:false});
+        $scope.TodoText = '';
+    };
 });
